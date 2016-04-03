6beed2e0a2da2ae5c848cb062e1dbb1959191177
DheerajThanySwarup <dheerajthany2@gmail.com>
2016-04-03 09:40:43 +0530

    initial commit

---
+++ TWAirLine-4/src/main/java/com/twair/Flight.java
@@ -0,0 +1,84 @@
+package com.twair;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Flight {
+    private String source;
+    private String destination;
+    private Plane plane;
+    private String number;
+    private final Integer availableSeats;
+    private Calendar departureTime;
+    private Calendar arrivalTime;
+    private Map<ClassType, TravelClass> travelClassMap = new HashMap<>();
+    private Double price;
+
+    public Flight(String number, String source, String destination, Plane plane, Calendar departure, Calendar arrival, List<TravelClass> travelClasses) throws Exception {
+        this.source = source;
+        this.destination = destination;
+        this.plane = plane;
+        this.number = number;
+        this.availableSeats = plane.getNumberOfSeats();
+        setScheduleTime(departure, arrival);
+        for(TravelClass travelClass : travelClasses) {
+            travelClassMap.put(travelClass.getClassType(), travelClass);
+        }
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public boolean canBook(ClassType classType, Integer numberOfSeats) {
+        if(travelClassMap.containsKey(classType)) {
+            return travelClassMap.get(classType).canBook(numberOfSeats);
+        }
+        return false;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Calendar getDepartureTime() {
+        return departureTime;
+    }
+
+    public Calendar getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public boolean hasClass(ClassType classType) {
+        return travelClassMap.containsKey(classType);
+    }
+
+    private void setScheduleTime(Calendar departureTime, Calendar arrivalTime) throws Exception {
+        if(departureTime.after(arrivalTime)) {
+            throw new Exception("departure time cannot be greater than arrival time");
+        }
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public double getPrice(ClassType classType, int noOfSeats) {
+        if(travelClassMap.containsKey(classType)) {
+            return travelClassMap.get(classType).getBasePrice()*noOfSeats;
+        }
+        return 0;
+
+    }
+}
